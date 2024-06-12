ee5f38451942c72faed754062f11e47beb31d2c3
Samuel <shmenzala@gmail.com>
2024-06-11 21:28:38 -0500

    [MODIFY] Format the code in the main class

--- src/main/java/sh/com/pe/ExpenseManagement/ExpenseManagementApplication.java
+++ src/main/java/sh/com/pe/ExpenseManagement/ExpenseManagementApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExpenseManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExpenseManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseManagementApplication.class, args);
+    }
 
 }
