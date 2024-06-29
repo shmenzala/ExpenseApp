0ce365410e72eea846088156e04d6537ff8775e9
Samuel <shmenzala@gmail.com>
2024-06-29 12:15:00 -0500

    [ADD] Añadir nuevo manejador de excepcion basado en argumentos no válidos (capturar los campos erróneos)

--- src/main/java/sh/com/pe/ExpenseManagement/exceptions/GlobalExceptionHandler.java
+++ src/main/java/sh/com/pe/ExpenseManagement/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sh.com.pe.ExpenseManagement.exceptions;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -29,5 +28,16 @@ public class GlobalExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), LocalDateTime.now(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorRequestBodyResponse> methodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest webRequest) {
+        Map<String, String> fieldsEx = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            fieldsEx.put(error.getField(), error.getDefaultMessage());
+        });
+
+        ErrorRequestBodyResponse errorResponse = new ErrorRequestBodyResponse(fieldsEx, LocalDateTime.now(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
 }
