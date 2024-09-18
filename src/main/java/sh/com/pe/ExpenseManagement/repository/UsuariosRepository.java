b7f41fdb32aa9424eb62403e6f9c5ae2977f369b
Samuel <shmenzala@gmail.com>
2024-09-18 16:58:53 -0500

    [MODIFY] Añadir método para búsqueda de usuario por username o email

--- src/main/java/sh/com/pe/ExpenseManagement/repository/UsuariosRepository.java
+++ src/main/java/sh/com/pe/ExpenseManagement/repository/UsuariosRepository.java
@@ -1,5 +1,6 @@
 package sh.com.pe.ExpenseManagement.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sh.com.pe.ExpenseManagement.model.Usuarios;
@@ -12,5 +13,7 @@ import sh.com.pe.ExpenseManagement.model.Usuarios;
 public interface UsuariosRepository extends JpaRepository<Usuarios, Integer> {
 
     public boolean existsByEmail(String email);
+    
+    public Optional<Usuarios> findByUsernameOrEmail(String username, String email);
     
 }
