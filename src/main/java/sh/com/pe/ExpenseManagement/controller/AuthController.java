c8044de9acc1fd6ce7cc2db43c0694cdedd35d2e
Samuel <shmenzala@gmail.com>
2024-09-18 17:09:32 -0500

    [ADD] Añadir clase controller para el manejo de endpoints referentes a la autenticación del usuario (register y authenticate)

---
+++ src/main/java/sh/com/pe/ExpenseManagement/controller/AuthController.java
@@ -0,0 +1,40 @@
+package sh.com.pe.ExpenseManagement.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sh.com.pe.ExpenseManagement.dto.JwtAuthenticationDto;
+import sh.com.pe.ExpenseManagement.dto.LoginDtoRequest;
+import sh.com.pe.ExpenseManagement.dto.RegisterDtoRequest;
+import sh.com.pe.ExpenseManagement.service.AuthenticationService;
+
+/**
+ *
+ * @author shmen
+ */
+@RestController
+@RequestMapping("/api/v1/auth")
+@CrossOrigin(origins = "http://localhost:4200")
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<JwtAuthenticationDto> registerUser(@Valid @RequestBody RegisterDtoRequest dto) {
+        return ResponseEntity.ok(authenticationService.register(dto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtAuthenticationDto> authenticateUser(@Valid @RequestBody LoginDtoRequest dto) {
+        return ResponseEntity.ok(authenticationService.authenticate(dto));
+    }
+
+}
