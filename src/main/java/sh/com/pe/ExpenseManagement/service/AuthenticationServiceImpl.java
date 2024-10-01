f48b2baec73120bbfb19570090e092396b94de2f
Samuel <shmenzala@gmail.com>
2024-10-01 13:37:09 -0500

    [MODIFY] Añadir métodos para el guardado y revocación de tokens durante el proceso de autenticación

--- src/main/java/sh/com/pe/ExpenseManagement/service/AuthenticationServiceImpl.java
+++ src/main/java/sh/com/pe/ExpenseManagement/service/AuthenticationServiceImpl.java
@@ -1,12 +1,14 @@
 package sh.com.pe.ExpenseManagement.service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import sh.com.pe.ExpenseManagement.configuration.TokenType;
 import sh.com.pe.ExpenseManagement.dto.JwtAuthenticationDto;
 import sh.com.pe.ExpenseManagement.dto.LoginDtoRequest;
 import sh.com.pe.ExpenseManagement.dto.RegisterDtoRequest;
@@ -14,9 +16,11 @@ import sh.com.pe.ExpenseManagement.exceptions.ResourceAlreadyExistsException;
 import sh.com.pe.ExpenseManagement.exceptions.ResourceNotFoundException;
 import sh.com.pe.ExpenseManagement.model.Personas_perfil;
 import sh.com.pe.ExpenseManagement.model.Roles;
+import sh.com.pe.ExpenseManagement.model.Token;
 import sh.com.pe.ExpenseManagement.model.Usuarios;
 import sh.com.pe.ExpenseManagement.repository.Personas_perfilRepository;
 import sh.com.pe.ExpenseManagement.repository.RolesRepository;
+import sh.com.pe.ExpenseManagement.repository.TokenRepository;
 import sh.com.pe.ExpenseManagement.repository.UsuariosRepository;
 import sh.com.pe.ExpenseManagement.security.JwtTokenProvider;
 
@@ -39,13 +43,16 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final AuthenticationManager authenticationManager;
 
-    public AuthenticationServiceImpl(UsuariosRepository usuariosRepository, PasswordEncoder passwordEncoder, RolesRepository rolesRepository, Personas_perfilRepository personas_perfilRepository, JwtTokenProvider jwtTokenProvider, AuthenticationManager authenticationManager) {
+    private final TokenRepository tokenRepository;
+
+    public AuthenticationServiceImpl(UsuariosRepository usuariosRepository, PasswordEncoder passwordEncoder, RolesRepository rolesRepository, Personas_perfilRepository personas_perfilRepository, JwtTokenProvider jwtTokenProvider, AuthenticationManager authenticationManager, TokenRepository tokenRepository) {
         this.usuariosRepository = usuariosRepository;
         this.passwordEncoder = passwordEncoder;
         this.rolesRepository = rolesRepository;
         this.personas_perfilRepository = personas_perfilRepository;
         this.jwtTokenProvider = jwtTokenProvider;
         this.authenticationManager = authenticationManager;
+        this.tokenRepository = tokenRepository;
     }
 
     @Override
@@ -80,6 +87,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         Usuarios nuevoUsuario = usuariosRepository.save(usuario);
 
         String token = jwtTokenProvider.generateToken(nuevoUsuario);
+
+        saveCurrentUserToken(token, nuevoUsuario);
         
         return new JwtAuthenticationDto(token);
     }
@@ -97,8 +106,38 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
 
         String token = jwtTokenProvider.generateToken(usuario);
+
+        revokeAllUserTokens(usuario);
+        saveCurrentUserToken(token, usuario);
         
         return new JwtAuthenticationDto(token);
+    }
+
+    private void saveCurrentUserToken(String token, Usuarios usuario) {
+        Token newToken = new Token();
+
+        newToken.setToken(token);
+        newToken.setToken_type(TokenType.BEARER);
+        newToken.setExpired(0);
+        newToken.setRevoked(0);
+        newToken.setUsuarios(usuario);
+
+        tokenRepository.save(newToken);
+    }
+    
+    private void revokeAllUserTokens(Usuarios usuario){
+        List<Token> validTokens = tokenRepository.findAllValidTokensByUsuarios(usuario.getId());
+        
+        if (validTokens.isEmpty()){
+            return;
+        }
+        
+        validTokens.forEach(vt -> {
+            vt.setExpired(1);
+            vt.setRevoked(1);
+        });
+        
+        tokenRepository.saveAll(validTokens);
     }
 
 }
