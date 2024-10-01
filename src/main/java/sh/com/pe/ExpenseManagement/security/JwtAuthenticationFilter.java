0f248b1a1bc057ed7b419b72dd18ae62d0a74c37
Samuel <shmenzala@gmail.com>
2024-10-01 13:39:29 -0500

    [MODIFY] Añadir nueva validación previo al establecimiento de la autenticación del usuario

--- src/main/java/sh/com/pe/ExpenseManagement/security/JwtAuthenticationFilter.java
+++ src/main/java/sh/com/pe/ExpenseManagement/security/JwtAuthenticationFilter.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import sh.com.pe.ExpenseManagement.repository.TokenRepository;
 
 /**
  *
@@ -23,35 +24,43 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final CustomUserDetailsService customUserDetailsService;
 
-    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider, CustomUserDetailsService customUserDetailsService) {
+    private final TokenRepository tokenRepository;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider, CustomUserDetailsService customUserDetailsService, TokenRepository tokenRepository) {
         this.jwtTokenProvider = jwtTokenProvider;
         this.customUserDetailsService = customUserDetailsService;
+        this.tokenRepository = tokenRepository;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        
+
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String username;
-        
-        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
-        
+
         jwt = authHeader.substring(7);
         username = jwtTokenProvider.obtainUsernameFromJwt(jwt);
-        
-        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
-            if(jwtTokenProvider.isTokenValid(jwt, userDetails)){
+
+            boolean isTokenValid = tokenRepository.findByToken(jwt)
+                    .map(t -> t.getExpired() != 1 && t.getRevoked() != 1)
+                    .orElse(false);
+
+            if (jwtTokenProvider.isTokenValid(jwt, userDetails) && isTokenValid) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-        
+
         filterChain.doFilter(request, response);
     }
 
